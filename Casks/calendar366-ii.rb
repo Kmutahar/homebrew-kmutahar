3e89746ea4906e2c2e74cea64910d17bea6a19c4
Khaldoon Mutahar <khal@mutahar.me>
2019-03-21 18:42:54 +0800

    Create calendar366-ii.rb
    Still not fully functional

---
+++ Casks/calendar366-ii.rb
@@ -0,0 +1,10 @@
+cask 'calendar366-ii' do
+  version '2.6.0'
+  sha256 '44f297342e5a29486c4ef21b246c073ec59928de52bd4b9b3939fee8a919e424'
+
+  url "https://nspektor.com/downloads/Calendar366_v#{version}.dmg"
+  name 'Calendar 366 II'
+  homepage 'https://nspektor.com/calendar366/mac/'
+
+  app 'Calendar 366 II.app'
+end
