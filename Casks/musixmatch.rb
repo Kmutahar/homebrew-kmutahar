5ea04f79882be2bed2d43c804983cdb9f2c2d920
Khaldoon Mutahar <khal@mutahar.me>
2019-04-20 12:52:18 +0800

    Update Musixmatch v3.1.1

--- Casks/musixmatch.rb
+++ Casks/musixmatch.rb
@@ -1,6 +1,6 @@
 cask 'musixmatch' do
-  version '0.21.27'
-  sha256 'cb4f789b2e50ca89ffa1b135ec579c249ade4b513c9256958194cfe4661b9325'
+  version '3.1.1'
+  sha256 'bd78c07a8ccc3b90a0b9e63de546e59792ec95946f9c668a240b79740ee304e8'
 
   url "https://download-app.musixmatch.com/download/Musixmatch-#{version}.dmg"
   appcast 'https://www.corecode.io/cgi-bin/check_urls/check_url_filename.cgi?url=http://download-app.musixmatch.com/download/osx'
